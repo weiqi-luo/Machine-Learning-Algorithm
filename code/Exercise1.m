c3d33330e0e7675768cfa5cbc6c9f07d661e3277
Weiqi Luo <weiqiluo0213@outlook.com>
2019-07-01 09:09:10 +0200

    second

--- code/Exercise1.m
+++ code/Exercise1.m
@@ -1,13 +1,25 @@
 function [par] = Exercise1(k)
 %EXCERCISE1 Summary of this function goes here
 %   Detailed explanation goes here
-data = load('Data.mat')
+load('Data.mat')
 for j = 1:6
-    for i = 1:10
-        index = zeros(1,size(Input,2))
+    for i = 1:k
+        %% Split the dataset
+        n = size(Input,2)
+        index = boolean(zeros(1,n))
+        index( (1+(i-1)*n/k) : (i*n/k) ) = 1
         input_test = Input(:,index)
         output_test = Output(:,index)
-        index = 
+        index = ~index
         input_train = Input(:,index)
         output_train = Output(:,index)
+        %% Linear regression
+        %% Compute the error
+    end
+    %% compute the average error
 end
+%% select p1 and p2 which minimize the position error and orientation error respectively
+%% Linear regression 
+end
+
+
