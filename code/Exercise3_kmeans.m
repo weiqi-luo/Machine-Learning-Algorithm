d18abbb53742f2cac71d23311a63a89db1f6c26a
Weiqi Luo <weiqiluo0213@outlook.com>
2019-07-02 19:41:57 +0200

    kmeans

--- code/Exercise3_kmeans.m
+++ code/Exercise3_kmeans.m
@@ -1,7 +1,20 @@
-function [outputArg1,outputArg2] = Exercise3_kmeans(inputArg1,inputArg2)
+function [outputArg1,outputArg2] = Exercise3_kmeans(inputArg1,k)
 %EXERCISE3_KMEANS Summary of this function goes here
 %   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
+clc
+clear
+load ('gesture_dataset.mat')
+ 
+
 end
 
+
+function plot_clusters(k)
+color = ['b','k','r','g','m','y','c'];
+figure
+hold on
+for i = 1:k
+    plot(gesture_l(:,i,1),gesture_l(:,i,2),'*',color(k));
+end
+hold off
+end
